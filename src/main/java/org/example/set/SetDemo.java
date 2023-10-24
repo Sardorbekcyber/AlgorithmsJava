5b127dba692d0b9267800e9a6f8eb4f9f4ad620a
Sardor Narziev <sardornarziyev2000@gmail.com>
2023-10-25 01:44:28 +0500

    [ADD] SetDemo.java

---
+++ src/main/java/org/example/set/SetDemo.java
@@ -0,0 +1,14 @@
+package org.example.set;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class SetDemo {
+
+    public static void main(String[] args) {
+        Set<Integer> nums = new HashSet<>();
+
+//        nums.add()
+    }
+
+}
