b060752fb51d1a39f06443133dde5e96e8271a4b
Sardor Narziev <sardornarziyev2000@gmail.com>
2023-12-19 00:18:09 +0500

    [ADD] Backtracking

---
+++ src/main/java/org/example/backtracking/NQueensMain.java
@@ -0,0 +1,9 @@
+package org.example.backtracking;
+
+public class NQueensMain {
+    public static void main(String[] args) {
+        NQueens nQueens = new NQueens(4);
+
+        nQueens.placeNQueens();
+    }
+}
